706e002a22687fc4659f89f10b5ee94534d12bb8
Arafat <arafatahmedtanimcsedu57@gmail.com>
2025-01-16 00:04:26 +0600

    update

--- webpack.qa.js
+++ webpack.qa.js
@@ -4,26 +4,31 @@ const { merge } = require('webpack-merge');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const PACKAGE = require('./package.json');
-const version = PACKAGE.version;
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
-	devtool: 'source-map',
+	devtool: 'inline-source-map',
 	mode: 'development',
 	output: {
-		path: path.join(__dirname, 'build'),
-		filename: `bundle-${version}.js`,
+		path: path.join(__dirname, 'public/dist'),
+		filename: `bundle.js`,
 		publicPath: '/',
 	},
-	optimization: {
-		minimizer: [new CssMinimizerPlugin()],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+		port: 8081,
+		hot: true,
+		historyApiFallback: true,
 	},
 	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('development'),
+		}),
 		new HtmlWebpackPlugin({
 			title: 'Form Builder',
 			template: './public/index.html',
-			minify: true,
+			minify: false,
 		}),
 		new Dotenv({
 			path: './.env.qa',
