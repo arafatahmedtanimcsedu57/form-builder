ca8abbcc4aae828017497622ee6488ff66b36d42
K.H.Tamim <tamim522@gmail.com>
2025-01-16 21:05:24 +0600

    stage webpack config and stage build command updated

--- webpack.staging.js
+++ webpack.staging.js
@@ -4,27 +4,35 @@ const { merge } = require('webpack-merge');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PACKAGE = require("./package.json");
+const version = PACKAGE.version;
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
 	devtool: 'inline-source-map',
 	mode: 'development',
 	output: {
-		path: path.join(__dirname, 'public/dist'),
-		filename: `bundle.js`,
+		// path: path.join(__dirname, 'public/dist'),
+    path: path.join(__dirname, "build"),
+		// filename: `bundle.js`,
+    filename: `bundle-${version}.js`,
 		publicPath: '/',
 	},
-	devServer: {
-		static: {
-			directory: path.join(__dirname, 'public'),
-		},
-		port: 8081,
-		hot: true,
-		historyApiFallback: true,
-	},
+	optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+	// devServer: {
+	// 	static: {
+	// 		directory: path.join(__dirname, 'public'),
+	// 	},
+	// 	port: 8081,
+	// 	hot: true,
+	// 	historyApiFallback: true,
+	// },
 	plugins: [
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('development'),
-		}),
+		// new webpack.DefinePlugin({
+		// 	'process.env.NODE_ENV': JSON.stringify('development'),
+		// }),
 		new HtmlWebpackPlugin({
 			title: 'Form Builder',
 			template: './public/index.html',
