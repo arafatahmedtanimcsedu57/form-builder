8ed150cb4835e021e8b7b60a5b3fcf8bab25d1fb
arafatahmedtanimcsedu57 <arafatahmedtanimcsedu57@gmail.com>
2025-09-27 11:30:06 +0600

    update

--- src/components/FormTemplates/NewFormDialogComponent.tsx
+++ src/components/FormTemplates/NewFormDialogComponent.tsx
@@ -9,12 +9,20 @@ import { useNavigate } from "react-router-dom";
 import { Dialog, DialogContent, TextField } from "@mui/material";
 
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { addTemplate } from "../../redux/entities/formBuilderEntity";
+import {
+  addTemplate,
+  getAllTemplates,
+  hasAlready,
+} from "../../redux/entities/formBuilderEntity";
 import { getToken, setToken } from "../../redux/auth/token";
 import { getLogin } from "../../redux/auth/login";
 
 import useModalStrip from "../../global-hooks/useModalStrip";
 import { TemplateType } from "../../types/FormTemplateTypes";
+import {
+  FormPaginationType,
+  GetAllTemplatesRequest,
+} from "../../types/ResponseFormTypes";
 
 interface NewFormDialogComponentProps {
   openDialog: boolean;
@@ -52,8 +60,8 @@ const NewFormDialogComponent: FunctionComponent<
       const { data: _data } = await dispatch(getLogin(data)).unwrap();
       dispatch(setToken(_data));
       props.setOpenDialog(false);
-    } catch {
-      props.setOpenDialog(false);
+    } catch (ex) {
+      showModalStrip("danger", "Invalid credentials", 5000);
     }
   };
 
@@ -67,14 +75,29 @@ const NewFormDialogComponent: FunctionComponent<
     setCreatingForm(true);
 
     try {
+      // Check if formId already exists
+      const request: GetAllTemplatesRequest = {
+        formId: newFormData.formId,
+        page: 0,
+        size: 1,
+      };
+      const { payload }: { payload: any } = await dispatch(hasAlready(request));
+
+      console.log(payload);
+      if (payload.numberOfElements > 0) {
+        throw new Error("Form ID already exists");
+      }
+
       const template: TemplateType = await dispatch(
         addTemplate(newFormData)
       ).unwrap();
       navigate(`/formbuilder/${template.publishStatus}-${template.formId}`);
-    } catch (ex) {
+    } catch (ex: any) {
+      setCreatingForm(false);
+
       showModalStrip(
         "danger",
-        "Error occurred while creating a new Form",
+        ex.message || "Error occurred while creating a new Form",
         5000
       );
     }
@@ -94,112 +117,112 @@ const NewFormDialogComponent: FunctionComponent<
           props.setOpenDialog(false);
         }}
       >
-        {authToken ? (
-          <DialogContent className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Enter the following details</h5>
-            </div>
+        <DialogContent className="modal-content">
+          {!authToken ? (
+            <>
+              <div className="modal-header">
+                <h5 className="modal-title">Please Login</h5>
+              </div>
+              <div className="modal-body py-4">
+                <form onSubmit={handleLogin}>
+                  <div className="mb-3">
+                    <TextField
+                      size="small"
+                      label="User Name"
+                      name="username"
+                      className="form-control"
+                      id="userName"
+                      key="userName"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <TextField
+                      size="small"
+                      label="Password"
+                      type="password"
+                      name="password"
+                      id="password"
+                      key="password"
+                      className="form-control"
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-sm btn-primary px-4 fw-medium"
+                  >
+                    {loginLoading ? (
+                      <>
+                        <span
+                          className="spinner-border spinner-border-sm mr-2"
+                          role="status"
+                          aria-hidden="true"
+                        ></span>
+                        Loading
+                      </>
+                    ) : (
+                      "Log In"
+                    )}
+                  </button>
+                </form>
+              </div>
+            </>
+          ) : (
+            <DialogContent className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Enter the following details</h5>
+              </div>
 
-            <div className="modal-body py-4">
-              <form onSubmit={handleFormSubmit}>
-                <div className="mb-3">
-                  <TextField
-                    required
-                    size="small"
-                    label="Form Name"
-                    name="formName"
-                    id="formName"
-                    key="formName"
-                    className="form-control"
-                    defaultValue=""
-                  />
-                </div>
+              <div className="modal-body py-4">
+                <form onSubmit={handleFormSubmit}>
+                  <div className="mb-3">
+                    <TextField
+                      required
+                      size="small"
+                      label="Form Name"
+                      name="formName"
+                      id="formName"
+                      key="formName"
+                      className="form-control"
+                      defaultValue=""
+                    />
+                  </div>
 
-                <div className="mb-3">
-                  <TextField
-                    required
-                    size="small"
-                    label="Form ID"
-                    name="formId"
-                    id="formId"
-                    key="formId"
-                    className="form-control"
-                    defaultValue=""
-                  />
-                </div>
+                  <div className="mb-3">
+                    <TextField
+                      required
+                      size="small"
+                      label="Form ID"
+                      name="formId"
+                      id="formId"
+                      key="formId"
+                      className="form-control"
+                      defaultValue=""
+                    />
+                  </div>
 
-                <button
-                  type="submit"
-                  className="btn btn-sm btn-success fw-medium px-4"
-                  disabled={creatingForm}
-                >
-                  {creatingForm ? (
-                    <>
-                      <span
-                        className="spinner-border spinner-border-sm mr-2"
-                        role="status"
-                        aria-hidden="true"
-                      ></span>
-                      Loading
-                    </>
-                  ) : (
-                    "Create Form"
-                  )}
-                </button>
-              </form>
-            </div>
-          </DialogContent>
-        ) : (
-          <DialogContent className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Login</h5>
-            </div>
-
-            <div className="modal-body py-4">
-              <form onSubmit={handleLogin}>
-                <div className="mb-3">
-                  <TextField
-                    size="small"
-                    label="User Name"
-                    name="username"
-                    className="form-control"
-                    id="userName"
-                    key="userName"
-                  />
-                </div>
-
-                <div className="mb-3">
-                  <TextField
-                    size="small"
-                    label="Password"
-                    type="password"
-                    name="password"
-                    id="password"
-                    key="password"
-                    className="form-control"
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="btn btn-sm btn-primary px-4 fw-medium"
-                >
-                  {loginLoading ? (
-                    <>
-                      <span
-                        className="spinner-border spinner-border-sm mr-2"
-                        role="status"
-                        aria-hidden="true"
-                      ></span>
-                      Loading
-                    </>
-                  ) : (
-                    "Log In"
-                  )}
-                </button>
-              </form>
-            </div>
-          </DialogContent>
-        )}
+                  <button
+                    type="submit"
+                    className="btn btn-sm btn-success fw-medium px-4"
+                    disabled={creatingForm}
+                  >
+                    {creatingForm ? (
+                      <>
+                        <span
+                          className="spinner-border spinner-border-sm mr-2"
+                          role="status"
+                          aria-hidden="true"
+                        ></span>
+                        Loading
+                      </>
+                    ) : (
+                      "Create Form"
+                    )}
+                  </button>
+                </form>
+              </div>
+            </DialogContent>
+          )}
+        </DialogContent>
       </Dialog>
     </>
   );
